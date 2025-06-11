3c1f4a8912b01af37efe6cd017e7440decf95e0b
bradrezdev <bradrez@Mac-mini-de-Bryan-Nunez.local>
2025-06-10 20:19:13 -0600

    Trabajando en la página Home Page sin inicio de sesión

--- index/index.py
+++ index/index.py
@@ -3,31 +3,64 @@
 import reflex as rx
 from rxconfig import config
 from index.state import State
-
+from .signup import signup
+from .login import login
 
 def index() -> rx.Component:
     # Contenedor principal
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+    return rx.vstack(
+        
+        # Contenedor Header | Logo + Search, Login, Signup
+        rx.hstack(
+            
+            rx.image(src="/logotipo.png", width="200px", height="auto"),
+
+            rx.box(   
+                # Botón Búsqueda
+                rx.icon_button("search", border_radius="100px", width="48px", height="48px", margin_right="10px",),
+
+                # Botón Registrarse
+                rx.link(
+                    rx.button(
+                        "Registrarse",
+                        border_radius="100px",
+                        width="135px",
+                        height="48px",
+                    ),
+                    href="/signup",
+                ),
+
+                # Botón Iniciar sesión
+                rx.link(
+                    rx.button(
+                        "Iniciar sesión",
+                        bg="white",
+                        border_radius="100px",
+                        width="135px",
+                        height="48px",
+                        border="5px solid purple",
+                    ),
+                    href="/login",
+                ),
+                
+            # Propiedades contenedor botones Búsqueda, Registro e Inicio de sesión
+
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+
+        # Propiedades @Header
+        justify="between",
+        padding="4%",
+        width="100%",
         ),
-        rx.logo(),
+
+        # Propiedades contenedor principal
+        height="100vh",
+        max_width="1920",
+        width="100%",
     )
 
 
-app = rx.App()
-app.add_page(index)
+app = rx.App(theme=rx.theme(accent_color="amber"))
+app.add_page(index, title="Página de inicio")
+app.add_page(login, title="Iniciar sesión")
+app.add_page(signup, title="Crear nueva cuenta")
---
+++ index/signup.py
@@ -0,0 +1,119 @@
+"""Proyecto Final | Programación Avanzada | Sign up Page"""
+
+import reflex as rx
+from rxconfig import config
+from index.state import State
+
+def signup() -> rx.Component:
+    # Contenedor principal
+    return rx.flex(
+        
+        # Contenedor izquierdo | Formulario
+        rx.vstack(
+            
+            rx.image(src="/logotipo.png", width="200px", height="auto"),
+
+            # Formulario de inicio de sesión.
+            rx.form(
+                
+                # Inputs del formulario.
+                rx.vstack(
+
+                    rx.heading("Crea una cuenta", size="8",),
+
+                    rx.text("Únete a la red de apoyo más grande para estudiantes y programadores:"),
+                    
+                    rx.text("Nombre"),
+
+                    rx.input(
+                        placeholder="Escribe tu nombre y apellido",
+                        name="email",
+                        style={"border": "1px solid black"},
+                        border_radius="8px",
+                        height="40px",
+                        width="25vw",
+                    ),
+
+                    rx.text("Correo electrónico"),
+
+                    rx.input(
+                        placeholder="Escribe tu correo electrónico",
+                        name="password",
+                        style={"border": "1px solid black"},
+                        border_radius="8px",
+                        height="40px",
+                        width="25vw",
+                    ),
+
+                    rx.hstack(
+
+                        rx.vstack(
+                            rx.text("Contraseña"),
+
+                            rx.input(
+                                placeholder="Escribe tu contraseña",
+                                name="password",
+                                style={"border": "1px solid black"},
+                                border_radius="8px",
+                                height="40px",
+                                width="100%",
+                            ),
+                        ),
+
+                        rx.vstack(
+                            rx.text("Confirmar contraseña"),
+
+                            rx.input(
+                                placeholder="Confirma tu contraseña",
+                                name="password",
+                                style={"border": "1px solid black"},
+                                border_radius="8px",
+                                height="40px",
+                                width="100%",
+                            ),
+                        ),
+
+                    ),
+
+                    rx.hstack(
+
+                        rx.checkbox("Al registrar los detalles de tu cuenta, aceptas nuestros términos y condiciones.", size="2"),
+
+                    ),
+
+                    rx.button("Registrarse", height="47px", width="25vw", border_radius="8px",),
+
+                    rx.hstack(
+                        rx.spacer(),
+                        rx.text("Ya tienes una cuenta?",size="1"),
+                        rx.link("Inicia sesión", href="/login",size="1"),
+                        rx.spacer(),
+                    # Propiedades de hstack "¿NO tienes una cuenta?
+                    spacing="1",
+                    width="100%",
+                    ),
+                ),
+
+                # Propiedades @Formulario de registro.
+                padding="20%",
+                width="100%",
+            ),
+
+            # Propiedades @Contenedor izquierdo | Formulario
+            #bg="blue",
+            justify="center",
+            padding="4%",
+            width="50%",
+        ),
+
+        # Contenedor derecho | Imagen
+        rx.box(
+            bg="red",
+            width="50%",
+        ),
+
+        # Propiedades contenedor principal
+        height="100vh",
+        max_width="1920",
+        width="100%",
+    )
